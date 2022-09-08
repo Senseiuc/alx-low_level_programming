edeea0d4cbfb1bc1077fdecefb67b9be6cc247d4
senseiuc <dhinhojboy@gmail.com>
2022-09-08 21:54:56 +0000

    without printf and puts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints a string without using printf or puts functions
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char Mystr[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, Mystr,59);
+	return (1);
+}
