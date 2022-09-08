7d4a7f12d56c544ac704bbb776619f7425921592
senseiuc <dhinhojboy@gmail.com>
2022-09-08 21:57:56 +0000

    without printf and puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,8 +10,9 @@
  */
 int main(void)
 {
-	char Mystr[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char Mystr[] = "and that piece of art is useful\"
+		- Dora Korpar, 2015-10-19\n";
 
-	write(1, Mystr,59);
+	write(1, Mystr, 59);
 	return (1);
 }
