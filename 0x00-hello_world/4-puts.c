eaf0efc5eead44b29e3bedc7056cc43affb469e2
senseiuc <dhinhojboy@gmail.com>
2022-09-08 16:41:07 +0000

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints a string using put
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char Mystr[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(Mystr);
+	return (0);
+}
