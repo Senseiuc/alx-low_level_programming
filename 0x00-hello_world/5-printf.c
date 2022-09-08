8e3c6f1e02a9b17fe40f773091b9b18ecbed2245
senseiuc <dhinhojboy@gmail.com>
2022-09-08 17:03:23 +0000

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints a string using put
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char Mystr[] = "with proper grammar, but the outcome is a piece of art,\n";
+
+	printf("%s", Mystr);
+	return (0);
+}
