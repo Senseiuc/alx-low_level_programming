a7d77abf39aec3c2d38d70dccf39803eb0272711
senseiuc <dhinhojboy@gmail.com>
2022-09-09 16:51:37 +0000

    Hexa

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints all digits
+ *
+ * Return: 0 always (Success)
+ */
+int main(void)
+{
+	int a = 0;
+	char c;
+
+	c = 'a';
+	while (a < 16)
+	{
+		if (a < 10)
+			putchar(a + '0');
+		else
+			putchar(c + (a % 10));
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
