18bf3a3a8fbe6bf65b991e73be8d444fa189db95
senseiuc <dhinhojboy@gmail.com>
2022-09-14 01:03:03 +0000

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include <math.h>
+
+/**
+ * _abs - converts to absolute value
+ *@c: takes in a c argument
+ *
+ * Description: Absolute value
+ *
+ * Return: an integer
+ */
+
+int _abs(int c)
+{
+	return (abs(c));
+
+}
