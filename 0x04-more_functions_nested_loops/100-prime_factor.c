5396afe416619bb910813c5e8ca002594816c0e2
senseiuc <dhinhojboy@gmail.com>
2022-09-15 23:20:33 +0000

    prime numbers

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -10,15 +10,15 @@ int main(void)
 	unsigned long int a = 612852475143;
 	int l_p_f = 2, i = 2;
 
-	while(a!=1)
+	while (a != 1)
 	{
-		while(a%i==0)
+		while (a % i == 0)
 		{
-			a = a/i;
+			a = a / i;
 			l_p_f = i;
 		}
 		i++;
 	}
-	printf("%d\n",l_p_f);
+	printf("%d\n", l_p_f);
 	return (0);
 }
