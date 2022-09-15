28357b72a837fecee98ea6ae11cf5bcb71dbe7cd
senseiuc <dhinhojboy@gmail.com>
2022-09-15 17:30:02 +0000

    squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * print_square - prints diagonal
+ * @n: integer n
+ *
+ * Return: void
+ */
+void print_square(int n)
+{
+	int a = 0, b = 0;
+	while (a < n)
+	{
+		b = 0;
+		while (b < n)
+		{
+			putchar('#');
+			b++;
+		}
+		putchar('\n');
+		a++;
+	}
+}
