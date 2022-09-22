5eb45a5f1bf9a78be6b1548fc0c02fbd7f5730d4
senseiuc <dhinhojboy@gmail.com>
2022-09-22 10:12:02 +0000

    Capitalize

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,5 +1,28 @@
 #include "main.h"
 /**
+ * is_seperator - checks if a character is a seperator
+ * @c:takes a character as arguement
+ *
+ * Return: 1 for true, 0 for false
+ */
+
+int is_seperator(char c);
+
+int is_seperator(char c)
+{
+	char sep[] = ",;.!?\"(){} \t\n";
+	int i = 0;
+
+	while (sep[i] != '\0')
+	{
+		if (sep[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+/**
  * cap_string - coverts strings to uppercase
  * @str: a string
  *
@@ -13,7 +36,7 @@ char *cap_string(char *str)
 
 	while (str[a] != '\0')
 	{
-		if (!((prev  >= 'a' && prev <= 'z') || (prev >= 'A' && prev <= 'Z')))
+		if (is_seperator(prev) == 1)
 			if (str[a] >= 'a' && str[a] <= 'z')
 				str[a] -= 32;
 		prev = str[a];
