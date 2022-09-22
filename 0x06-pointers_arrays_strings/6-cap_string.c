20eaae599fe87fb0b5374e89149144cbb5f181af
senseiuc <dhinhojboy@gmail.com>
2022-09-22 11:06:16 +0000

    capitalize fix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,15 +10,31 @@ int is_seperator(char c);
 
 int is_seperator(char c)
 {
-	char sep[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(',')', '{', '}'};
+	char sep[] = " \t\n,;.!?(){}";
 	int i = 0;
 
 	while (sep[i] != '\0')
 	{
-		if (sep[i] == c)
+		if (sep[i] == c || c == '"')
 			return (1);
 		i++;
 	}
+	return (0);
+}
+
+/**
+ * is_lower - check if it's lowercase'
+ * @a: a char as argument
+ *
+ * Return: 1 or 0
+ */
+
+int is_lower(char a);
+
+int is_lower(char a)
+{
+	if (a >= 'a' && a <= 'z')
+		return (1);
 	return (0);
 }
 
@@ -32,14 +48,16 @@ int is_seperator(char c)
 char *cap_string(char *str)
 {
 	int a = 0;
-	char prev = '1';
 
 	while (str[a] != '\0')
 	{
-		if (is_seperator(prev) == 1)
-			if (str[a] >= 'a' && str[a] <= 'z')
-				str[a] -= 32;
-		prev = str[a];
+		if (a == 0 && is_lower(str[a]) == 1)
+			str[a] -= 32;
+		if (is_seperator(str[a]) == 1)
+		{
+			if (is_lower(str[a + 1]) == 1)
+				str[a + 1] -= 32;
+		}
 		a++;
 	}
 	return (str);
