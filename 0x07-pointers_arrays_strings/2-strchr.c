7225573ca21d63c5fd2326f0fdaef0a12c499f2d
senseiuc <dhinhojboy@gmail.com>
2022-09-26 16:58:49 +0000

    find a char

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strchr - finds a char
+ *@s: an array of characters
+ *@c: a character
+ *
+ *Description: find a character
+ *
+ *Return: a pointer to the memory area s
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (*(s +i) != '\0')
+	{
+		if (*(s + i) == c)
+			return (s + i);
+		i++;
+	}
+	return (NULL);
+}
