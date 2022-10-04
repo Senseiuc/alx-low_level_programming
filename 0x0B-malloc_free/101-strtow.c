0de233cd526ad4baf8d5c0b16e1afae42661451e
senseiuc <dhinhojboy@gmail.com>
2022-10-04 20:00:38 +0000

    seperate string

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,84 @@
+#include "main.h"
+#include <stdlib.h>
+
+int get_string_count(char *str);
+/**
+ *get_string_count - gets the length of a string
+ *@str: the string to be counted
+ *
+ *Return: the address of the memory or null
+ */
+
+int get_string_count(char *str)
+{
+	int j = 0;
+
+	while (str[j] != '\0')
+		j++;
+	return (j);
+}
+
+/**
+  *strtow - split a string into multiple strings
+  *@str: the string to be split
+  *
+  *Return: the address of the memory of the new array or null
+  */
+
+char **strtow(char *str)
+{
+	char **newArray;
+	int j, k, found = 0, l, m, wc = 0, length = get_string_count(str);
+
+	if (str[0] == '\0' || str == NULL)
+	{
+		return (NULL);
+	}
+	for (j = 0; j < length; j++)/* calculate the number of words*/
+	{                      /* in the string given*/
+		if ((str[j] == ' ' || str[j] == '\0') && found == 1)
+		{
+			k++;
+			found = 0;
+		}
+		else if (str[j] != ' ' && str[j] != '\0')
+			found = 1;
+		else
+			found = 0;
+	}
+	newArray = (char **) malloc((k) * sizeof(char *));
+	if (newArray == NULL)
+		return (NULL);
+	k = 0; /* for iterating the array of pointers*/
+	found = 0;
+	for (j = 0; j < length; j++) /* iterate through the string to create*/
+	{                            /* arrays of strings seperated by space*/
+		if (str[j] != ' ' && str[j] != '\0')
+		{
+			found = 1;
+			wc++;
+		}
+		else if ((str[j] == ' ' || str[j] == '\0') && found == 1)
+		{
+			newArray[k] = (char *)malloc((wc + 1) * sizeof(char));
+			l = 0;
+			m = j - wc;
+			while ((m + l) < j)
+			{
+				newArray[k][l] = str[m + l];
+				l++;
+			}
+			newArray[wc] = '\0';
+			found = 0;
+			wc = 0;
+			k++;
+		}
+		else
+		{
+			found = 0;
+			wc = 0;
+		}
+
+	}
+	return (newArray);
+}
