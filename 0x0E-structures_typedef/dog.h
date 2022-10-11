771cd7bfeb9edadea896a9b4114ebee655cc7508
senseiuc <dhinhojboy@gmail.com>
2022-10-11 01:05:41 +0000

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+  * struct dog - a dog struct
+  *@name: name of the dog - string
+  *@age: age of the dog - float
+  *@owner: owner of the dog - string
+  *
+  *Description: A representation of the dog characteristics
+  */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
+
