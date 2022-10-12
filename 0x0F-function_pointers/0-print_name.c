264f327c1f4214dc070cf056b2a164dadc6c5f6e
senseiuc <dhinhojboy@gmail.com>
2022-10-12 16:32:54 +0000

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+  *print_name - prints a name
+  *@name: address of the string
+  *@f: address if the function f
+  *
+  *Return: void
+  */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
