7008ed00a7180640d9abdce24c806b2e4330ea5d
senseiuc <dhinhojboy@gmail.com>
2022-10-12 22:24:27 +0000

    arguments operation

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,59 @@
+/**
+ * op_add - add two integers
+ *@a: The first integer
+ *@b: The second integer
+ *
+ *Return: integer
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtract two integers
+ *@a: The first integer
+ *@b: The second integer
+ *
+ *Return: integer
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiply two integers
+ *@a: The first integer
+ *@b: The second integer
+ *
+ *Return: integer
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divide an integer with another
+ *@a: The first integer
+ *@b: The divisor
+ *
+ *Return: integer
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - find the mod B of int A
+ *@a: The first integer
+ *@b: The second integer
+ *
+ *Return: integer
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
