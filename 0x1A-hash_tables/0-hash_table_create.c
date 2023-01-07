9e22e0a6f42c8892f167eaab72035a0461ec59f5
senseiuc <dhinhojboy@gmail.com>
2023-01-07 18:07:49 +0000

    memory

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,13 +9,16 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table = (hash_table_t *) malloc(sizeof(hash_table_t));
+	hash_table_t *table = malloc(sizeof(hash_table_t));
 
-	if (!table)
+	if (table == NULL)
 		return (NULL);
 	table->size = size;
-	table->array = (hash_node_t **) malloc(table->size * sizeof(hash_node_t));
+	table->array = (hash_node_t **) malloc(size * sizeof(hash_node_t));
 	if (!table->array)
+	{
+		free(table);
 		return (NULL);
+	}
 	return (table);
 }
