402b20873faf5281b4a14a2027d16d0e52c93075
senseiuc <dhinhojboy@gmail.com>
2023-04-13 21:50:08 +0000

    advanced binary

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -11,4 +11,5 @@ int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int binary_search2(int *array, size_t left, size_t right, int value);
 int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
 #endif /*SEARCH_ALGOS_H*/
